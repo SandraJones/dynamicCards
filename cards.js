4d918479f1ee85c517471e2321bcfb7618935185
Sandy Jones <sandynashv@gmail.com>
2016-03-05 12:43:03 -0600

    declared variables

---
+++ cards.js
@@ -0,0 +1,24 @@
+Create an HTML page that contains a text area 
+and a button labeled Create.
+When the user enters in text into the text 
+area and then clicks the create button, 
+create a new card element in the DOM. You 
+decide the height/width of the card.
+When the user clicks the Delete button, 
+the containing card, and no other cards, 
+should then be removed from the DOM. Not 
+just made invisible, actually removed from 
+the DOM.
+//need a delete button within the card
+//card has to be a class because there will be more than one
+var createButton = document.getElementById("create");
+var deleteButton = document.getElementById("delete");
+//it will be a dynamic delete
+var input = document.getElementsByClassName("inputArea");
+//card will also be dynamic, created only  after a button is clicked
+
+var container = document.getElementById();
+var card = document.getElementsbyClassName("cardDiv"); 
+
+
+
